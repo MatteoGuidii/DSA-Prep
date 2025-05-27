24106966e9a6a6aff07e5e0c2b3b0b2f7d9722d3
Matteo Guidi <66534314+MatteoGuidii@users.noreply.github.com>
2025-05-27 00:22:33 -0400

    Time: 4 ms (48.52%) | Memory: 18.4 MB (90.10%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -11,3 +11,10 @@ class Solution:
                 right -= 1
             else:
                 left += 1
+
+# Time Complexity: O(1)
+# We use a two-pointer approach on the sorted list.
+# In the worst case, each pointer moves at most n steps total, performing constant work per iteration.
+        
+# Space Complexity: O(1)
+# Only a fixed number of extra variables are used (left, right, total), regardless of input size.
